57a489d19fc56349deecdd003ff3fc2fc3f035cf
Streltsov_Mike <streltsovmike@yandex.ru>
2022-08-06 15:46:45 +0300

    Add program

---
+++ FindThreeSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] arrayString = new string[] { "привет", "как", "дела", "?", ";-)" };
+
+int amountOfSymbols = 0;
+int count = 0;
+
+for (int i = 0; i < arrayString.Length; i++)                 //Найдем количество ячеек с длиной строки <=3
+{
+    amountOfSymbols = arrayString[i].Length;
+    if (amountOfSymbols <= 3)
+    {
+        count++;
+    }
+}
+
+string[] arrayTreeSymbols = new string[count];               //Создадим массив с длинной равной count
+
+count = 0;
+
+for (int i = 0; i < arrayString.Length; i++)                 //Подставим нужные значения в новый массив
+{
+    amountOfSymbols = arrayString[i].Length;
+    if (amountOfSymbols <= 3)
+    {
+        arrayTreeSymbols[count] = arrayString[i];
+        count++;
+    }
+}
+
+var str = string.Join(", ", arrayTreeSymbols);
+Console.WriteLine($"[{str}]");
